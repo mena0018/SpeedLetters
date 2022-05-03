bf07538ef4149b217ae1fcde2336a74d80aa56dc
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-05-03 09:30:23 +0200

    Ajout de la génération d'une phrase à écrire

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,43 +1,38 @@
 import "../Styles/css/Main.css";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 
 export default function Main() {
 
-  const [areaValue, setAreaValue] = useState("");
-  const [sentence, setSentence] = useState();
+  const [areaValue, setAreaValue] = useState();
+  const [sentence, setSentence] = useState("");
   const APICALL = "http://api.quotable.io/random";
 
   const handleChange = (event) => setAreaValue(event.target.value);
-
 
   const displaySentence = async () => {
     const fetchDatas = await fetch(APICALL);
     const json = await fetchDatas.json();
     const resultats = json.content;
-
-
+    
     setSentence('');
-
-    // list = resultats.split('').map((caracter, index) => {
-    //   return <span key={index}> {caracter} </span>
-    // });
-
-  
-
+    setSentence(resultats)
+    setAreaValue('')
   };
-
+    
   useEffect(() => {
     displaySentence();
   }, []);
 
 
-
   return (
     <>    
       <div className="container">
-         <div className="sentencetoWrite">
-            {sentence}    
+         <div className="sentenceToWrite">
+            { sentence.split('').map((carac, index) => (
+                <span key={index}>{carac}</span>
+              ))
+            }    
          </div>
 
          <textarea value={areaValue}
@@ -49,3 +44,4 @@ export default function Main() {
     </>
   )
 }
+ 
